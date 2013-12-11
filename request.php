259795af5cb4583e19386f7c54e94f0eae856947
Flavien <Debflav@hotmail.com>
2013-12-11 16:06:04 +0100

    missing file request

---
+++ request.php
@@ -0,0 +1,11 @@
+<?php
+
+spl_autoload_register();
+
+
+$attributes = $_POST['attributes'];
+$dimension = $_POST['dimension'];
+
+$map = new MapGenerator\Map();
+$map->generate($dimension, array($attributes));
+echo $map->mapToJson();
