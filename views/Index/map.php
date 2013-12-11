912248dff97cfda6fd74854de4114c49626d4cae
Flavien <Debflav@hotmail.com>
2013-12-11 14:22:31 +0100

    -

--- views/Index/map.php
+++ views/Index/map.php
@@ -5,7 +5,8 @@
     <body>
         <script src="assets/js/jquery-2.0.3.min.js"></script>
         <script>var map = <?php echo $oMap; ?>;</script>
-        <script src="assets/js/canvas.js"></script>
+        <script src="assets/js/canvas_1.js"></script>
+        <script src="assets/js/rover.js"></script>
         <canvas id="canvas" >
             Canvas not supported.
         </canvas>
