d54164cfb7efbde1b3c81586bb0bb9a2688a47df
Lyokoï <lyokoi@free.fr>
2013-12-13 16:16:38 +0100

    travail sur l'index

---
+++ MapGenerator/fonction.php
@@ -0,0 +1,49 @@
+<?php
+
+
+	function EmptyMap ($Dimension) {
+
+		$MatriceVide = array_fill(0, $Dimension, array_fill(0, $Dimension, NULL));
+
+		return $MatriceVide;
+	}
+	
+	function ChoiceBlock ($Dimension) {
+
+		// Pondération d'apparition des blocs
+		$NatureBlock[ROCK] = 35;
+		$NatureBlock[SAND] = 35;
+		$NatureBlock[IRON] = 5;
+		$NatureBlock[ORE] = 9;
+		$NatureBlock[ICE] = 15;
+		$NatureBlock[OTHER] = 1;
+
+		// Définir la fonction rand() entre 0 et 100
+        $jet = rand(1, 100);
+
+
+        // fait un tri du résultat pout trouver la bonne nature
+        
+       	switch ($jet)
+       	{
+       	    case($jet <= $NatureBlock[0])
+       	    	return new Rock($Dimension); // on affecte au bloc la nature tirée au dé
+       	    	break;
+       	    case($jet <= $NatureBlock[0] + $NatureBlock[1])
+       	    	return new Sand($Dimension);
+       	    	break;
+       	    case($jet <= $NatureBlock[0] + $NatureBlock[1] + $NatureBlock[2])
+       	    	return new Iron($Dimension);
+       	    	break;
+       	    case($jet <= $NatureBlock[0] + $NatureBlock[1] + $NatureBlock[2] + $NatureBlock[3])
+       	    	return new Ore($Dimension);
+       	    	break;
+       	    case($jet <= $NatureBlock[0] + $NatureBlock[1] + $NatureBlock[2] + $NatureBlock[3] + $NatureBlock[4])
+       	    	return new Ice($Dimension);
+       	    	break;
+       	    case($jet <= $NatureBlock[0] + $NatureBlock[1] + $NatureBlock[2] + $NatureBlock[3] + $NatureBlock[4] + $NatureBlock[5])
+       	    	return new Iron($Dimension);
+       	    	break;
+      	}
+
+	}
---
+++ MapGenerator/index.php
@@ -0,0 +1,51 @@
+<?php
+
+	// On inclus les fonctions de l'index
+	include 'fonction.php';
+
+	// VARIABLES
+	$BlocXY = 5; // Nombre de blocs en longueur et largeur
+	$DimensionBloc = 25; // Nombre de cellule par bloc
+	$Bloc = array_fill(0, $BlocXY, array_fill(0, $BlocXY, NULL)); // tableau contenant des objets blocs
+
+	// ETAPE 0 :
+	// on génère la carte vide à partir des infos
+	$TheMap = EmptyMap($BlocXY * $DimensionBloc);
+
+	// ETAPE 1 :
+	// On génère la carte par bloc avec altitude pseudo plane et nature
+	for ($i = 0; $i < ($BlocXY); $i++) {
+
+		for ($j = 0; $j < ($BlocXY); $j++) { // colonnes de blocs
+
+			// Fonction de génération d'un bloc
+			$Bloc[$i][$j] = ChoiceBlock($DimensionBloc);
+			$Bloc[$i][$j]->generate();
+		}
+	}
+
+	// On remplit la carte vide avec les blocs
+	for ($i = 0; $i < ($BlocXY); $i++) { // ligne de blocs
+
+		for ($j = 0; $j < ($BlocXY); $j++) { // colonnes de blocs
+			
+			for ($k = 0; $k <= ($DimensionBloc); $k++) { // ligne du bloc
+
+				for ($l = 0; $l <= ($DimensionBloc); $l++) { // colonnes du bloc
+
+					$TheMap[][] = $Bloc[][]->;
+	}
+
+
+
+
+
+	// ETAPE 2 :
+	// on dépose sur la carte un certain nombre d'objet
+
+	// on définit le nombre d'objet à poser en fonction du nombre de bloc divisant la carte
+
+	// on pose les objets un à un
+
+	// ETAPE 3 :
+	// On créer le JSON
